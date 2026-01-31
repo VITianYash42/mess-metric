9ad8fe453b11c0ec50909c63a2964ba1f5ef0c02
2608raghav <guptaraghav2608@gmail.com>
2026-01-31 14:42:53 +0530

    Server req4 (#9)
    * made a commit
    
    * commit has been done
    
    * commit has been made
    
    * commit has made
    
    ---------
    
    Co-authored-by: Raghav <guptaraghav2608.com>

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
-  password: String
+  password: String,
+  mealCoins: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model("User", userSchema);
